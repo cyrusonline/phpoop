d8f009520976aa5bdf4617dee7b134fb026d9fbe
C.K. Chan <chanchikin@ied.edu.hk>
2015-07-06 11:57:35 +0800

    extend

--- index.php
+++ index.php
@@ -3,31 +3,66 @@
 class Baddie
 {
 	public $evilness = 10;
-	public $name;
+// 	public $name;
 	
 	
-	public function __construct($evilness, $name){
-		$this->evilness = $evilness;
-		$this->name = $name;
+// 	public function __construct($evilness, $name){
+// 		$this->evilness = $evilness;
+// 		$this->name = $name;
+		
+// 	}
+	
+	
+// 	public function __destruct(){
+// 		echo "A ".__CLASS__." has been destroyed";
+		
+		
+// 	}
+	
+// 	public function __toString(){
+// 		echo $this->name;
+// 	}
+	
+	
+// 	public function sayHi() {
+// 		echo "Hi";
+// 	}
+	
+// 	public function getEvilness(){
+// 		return $this->evilness;
+// 	}
+	
+// 	public function makeMoreEvil(){
+// 		++$this->evilness;
+// 		return $this->evilness;
+// 	}
+
+	public function increaseEvilness(){
+		++$this->evilness;
 		
 	}
-	public function sayHi() {
-		echo "Hi";
-	}
+
+}
+
+
+class Boss extends Baddie{
 	
-	public function getEvilness(){
-		return $this->evilness;
-	}
-	
-	public function makeMoreEvil(){
-		++$this->evilness;
-		return $this->evilness;
+	public $evilness = 50;
+	public function __destruct(){
+		echo "You beat the boss";
 	}
 }
 
 $ganon = new Baddie(8, "Baddie1");
-$gooo = new Baddie(99, "Soccer");
-var_dump($ganon);
+$gooo = new Boss();
+var_dump($gooo);
+//echo $ganon;
+
+unset($gooo);
+
+// unset($ganon);
+//$gooo = new Baddie(99, "Soccer");
+//var_dump($ganon);
 /*
  * echo $ganon->sayHi();
 echo "<br>";
@@ -37,12 +72,12 @@ echo $ganon->makeMoreEvil();
 
 $gooo = new Baddie;
  */
-echo "<br>";
-echo $ganon->evilness;
-echo $ganon->name;
-echo "<br>";
-echo $gooo->evilness;
-echo $gooo->name;
+// echo "<br>";
+// echo $ganon->evilness;
+// echo $ganon->name;
+// echo "<br>";
+// echo $gooo->evilness;
+// echo $gooo->name;
 
 
 ?>
